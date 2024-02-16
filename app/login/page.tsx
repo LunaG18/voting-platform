2b1b6ab549b8176aa6cf37f3a20b304a70360933
Lunathi Gwayi <GWYLUN001@myuct.ac.za>
2024-02-16 15:04:47 +0200

    login and register edits

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -38,7 +38,7 @@ export default function Login() {
                   autoComplete="email"
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-black text-center shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
 </div>
 </div>
@@ -59,7 +59,7 @@ export default function Login() {
                   autoComplete="current-password"
                   onChange={(e) => setPassword(e.target.value)}
                   required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-black text-center shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
 </div>
 </div>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -108,7 +108,7 @@ export default function Signup() {
                 onClick={() => signup()}
                 className="disabled:opacity-40 flex w-full justify-center rounded-md bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
               >
-                Sign Up
+                Register
               </button>
             </div>
           </div>
